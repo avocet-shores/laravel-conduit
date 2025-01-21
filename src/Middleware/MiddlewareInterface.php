2837b0fb22e712da4831ccac03a8ce3c5e0a8fca
Jared Cannon <jaredcannon9@gmail.com>
2025-01-21 14:29:08 -0600

    Add Middleware Interface

--- src/Middleware/MiddlewareInterface.php
+++ src/Middleware/MiddlewareInterface.php
@@ -2,7 +2,9 @@
 
 namespace AvocetShores\Conduit\Middleware;
 
+use AvocetShores\Conduit\Contexts\AIRequestContext;
+
 interface MiddlewareInterface
 {
-
+    public function handle(AIRequestContext $context, callable $next): AIRequestContext;
 }
