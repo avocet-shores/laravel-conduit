1cf3f32d9887efc0e5b950fcdede6cc2016c29ab
Jared Cannon <jaredcannon9@gmail.com>
2025-01-21 14:28:11 -0600

    Add missing role in fromString method and update namespace

--- src/Enums/Role.php
+++ src/Enums/Role.php
@@ -1,22 +1,38 @@
 <?php
 
-namespace JaredCannon\LaravelAI\Enums;
+namespace AvocetShores\Conduit\Enums;
+
+use Illuminate\Support\Str;
 
 enum Role: string
 {
+    /**
+     * Newer role for instructing reasoning models like o1
+     */
     case DEVELOPER = 'developer';
+
+    /**
+     * The system role is used by most models to give initial instructions to the model.
+     */
     case SYSTEM = 'system';
+
+    /**
+     * User messages are the main input to the model.
+     */
     case USER = 'user';
+
     case ASSISTANT = 'assistant';
     case FUNCTION = 'function';
 
     public static function fromString(string $role): self
     {
-        return match ($role) {
+        return match (Str::lower($role)) {
             'system' => self::SYSTEM,
             'user' => self::USER,
             'assistant' => self::ASSISTANT,
             'function' => self::FUNCTION,
+            'developer' => self::DEVELOPER,
+            default => 'user',
         };
     }
 }
